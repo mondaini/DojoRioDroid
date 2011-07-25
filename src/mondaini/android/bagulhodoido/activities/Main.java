98cd6247be57dfa75f1993485785109816022433
Filipe Mondaini <filipe@mondaini.me>
2011-07-25 18:43:04 -0300

    First commit

---
+++ src/mondaini/android/bagulhodoido/activities/Main.java
@@ -0,0 +1,20 @@
+package mondaini.android.bagulhodoido.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+	//TODO Criar Tab com os Endereços do local onde surgem os eventos
+	//TODO Criar Tab com as noticias do Blog	
+	//TODO Abrir ListActivity Aqui dentro.
+	//TODO Listar Notícias de Algum RSS (para mostrar na listactivity
+	//TODO Listar Endereços do Google Maps para abrir aqui no app.
+	
+}
